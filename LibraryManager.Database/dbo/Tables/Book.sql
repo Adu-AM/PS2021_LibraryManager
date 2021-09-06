bb78e7eda4855fa6c65f828a9dc430b487fc31d8
Adu-AM <75081484+Adu-AM@users.noreply.github.com>
2021-09-06 16:51:47 +0300

    Revert "post rezolvat."
    This reverts commit 204bbd75d3af6b0d33e6a3f2e6ac70c010c3a411.

--- LibraryManager.Database/dbo/Tables/Book.sql
+++ LibraryManager.Database/dbo/Tables/Book.sql
@@ -3,5 +3,5 @@
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [Name] NCHAR(100) NOT NULL,
     [Author] NCHAR(100) NOT NULL,
-    [Image] NCHAR(200) NOT NULL,
+    [Image] NCHAR(100) NOT NULL,
 )
